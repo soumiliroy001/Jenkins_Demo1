8615c62810bf38cd88a2378ceb3996ce96f5c0a2
Soumili_Roy <Soumili.Roy@skillassure.com>
2022-03-25 21:28:52 +0530

    1st commit

---
+++ src/test/java/com/demo/Demo_Google.java
@@ -0,0 +1,24 @@
+package com.demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo_Google {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		WebDriver driver;
+		driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		WebElement search=driver.findElement(By.name("q"));
+		search.sendKeys("Selenium");
+		search.sendKeys(Keys.ENTER);
+		driver.findElement(By.xpath("//h3[@class='LC20lb MBeuO DKV0Md']")).click();
+		
+	}
+
+}
